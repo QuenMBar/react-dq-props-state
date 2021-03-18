cb6c17002a330634e828ecb9249730c2d41d2e98
QuenMBar <Qbeast360@gmail.com>
2021-03-18 09:41:24 -0700

    wip

--- src/BabyHog.js
+++ src/BabyHog.js
@@ -1,47 +1,52 @@
-import React, { Component } from 'react'
-import { Button } from 'semantic-ui-react'
-import normalBaby from './assets/unadulterated-hoglette.png'
-import SunBaby from './assets/sun-eyes.png'
-import BlueBaby from './assets/blue-eyes.png'
-import GlowingBaby from './assets/glowing-eyes.png'
+import React, { Component } from "react";
+import { Button } from "semantic-ui-react";
+import normalBaby from "./assets/unadulterated-hoglette.png";
+import SunBaby from "./assets/sun-eyes.png";
+import BlueBaby from "./assets/blue-eyes.png";
+import GlowingBaby from "./assets/glowing-eyes.png";
 
-// is there a way we could associate eye color string values with images? 
+// is there a way we could associate eye color string values with images?
 // perhaps so we could do something along the lines of `eyeColorMapper['blue'] and get back the right image?`
 
 export default class BabyHog extends Component {
+    constructor(props) {
+        super(props);
+        console.log(props.eyeColor);
+        this.state = {
+            eyes: props.eyeColor,
+            hogData: props.hogData,
+            weight: 10,
+        };
+        // eyeColorMapper["blue"];
+    }
 
-  constructor(props) {
-    super(props)
-  }
+    changeWeight = (e) => {
+        // nothing needs to change here
+        const newWeight = e.target.name === "+" ? this.state.weight + 10 : this.state.weight - 10;
+        this.setState({
+            weight: newWeight,
+        });
+    };
 
-  changeWeight = (e) => {
-    // nothing needs to change here
-    const newWeight = e.target.name === "+" ? (this.state.weight + 10) : (this.state.weight - 10)
-    this.setState({
-      weight: newWeight
-    })
-  }
+    render() {
+        return (
+            <li className="hogbabies">
+                <h1>Name: {this.state.hogData["name"]}</h1>
+                <h3>Weight: {this.state.weight}</h3>
+                <h3>Hobby: {this.state.hogData["hobby"]}</h3>
+                <h4>Eye Color: {this.state.eyes}</h4>
 
-  render() {
-    return (
-      <li className="hogbabies">
-        <h1>Name</h1>
-        <h3>Weight:</h3>
-        <h3>Hobby:</h3>
-        <h4>Eye Color:</h4>
-          
-        <Button name="+">
-          Increase Weight
-        </Button>
-        <Button name="-">
-          Decrease Weight
-        </Button>
+                <Button onClick={this.changeWeight} name="+">
+                    Increase Weight
+                </Button>
+                <Button onClick={this.changeWeight} name="-">
+                    Decrease Weight
+                </Button>
 
-        <div className="hb-wrap">
-          <img src={normalBaby} style={{height: '200px'}} alt="MasterBlasterJrJr" />
-        </div>
-        
-      </li>
-    )
-  }
+                <div className="hb-wrap">
+                    <img src={normalBaby} style={{ height: "200px" }} alt="MasterBlasterJrJr" />
+                </div>
+            </li>
+        );
+    }
 }
